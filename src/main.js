179ae8ccccd136ff3370c17e6aaf352b01ab49ca
Jon Dale <jonners@gmail.com>
2017-10-26 23:29:29 -0400

    Better responsive/mobile presentation

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import debounce from 'lodash.debounce';
 import anime from 'animejs';
 
-let circlesPerTitle = 2;
+let baseCirclesPerTitle = 2;
 let titleStart = 2;
 let circleMargin = 20;
 
-function tempCircle(s, x, y, p, i){
+function makeCircle(s, x, y, p, i){
   var el = document.createElement('div');
   el.innerHTML =  `<div class="js-circle br-100 absolute circle"
                         style="width:${(s - p)}px;
@@ -19,14 +19,15 @@ function tempCircle(s, x, y, p, i){
 }
 
 function isNotTitleRow(i){
-  return (i < titleStart || i >= (titleStart + circlesPerTitle));
+  return (i < titleStart || i >= (titleStart + getCirclesPerTitle()));
 }
 
-function renderCircles(size){
-  let rows = 6;
+function renderCircles(){
+  let size = getCircleSize();
+  let rows = 4 + getCirclesPerTitle();
   let sectionEl = document.querySelector('.js-container');
   let titleEl = document.querySelector('.js-title');
-  let titleOffset = 3 * (titleEl.offsetWidth / 7);
+  let titleOffset = 2 * (titleEl.offsetWidth / 5);
   let perRow = Math.floor((sectionEl.offsetWidth - titleOffset) / size);
 
   for(let i=0; i<rows; i++){
@@ -34,7 +35,7 @@ function renderCircles(size){
       // Don't draw circles over the heading.
       if(isNotTitleRow(i) ||
          (!isNotTitleRow(i) && ((j * size) < (sectionEl.offsetWidth - titleEl.offsetWidth)))) {
-        sectionEl.appendChild(tempCircle(size, sectionEl.offsetWidth - (j * size), i * size, circleMargin, anime.random(1, 40)));
+        sectionEl.appendChild(makeCircle(size, sectionEl.offsetWidth - (j * size), i * size, circleMargin, anime.random(1, 40)));
       }
     }
   }
@@ -76,21 +77,39 @@ function setupLinks(){
   });
 }
 
+function getCirclesPerTitle(){
+  let num;
+  let height = document.querySelector('.js-title').offsetHeight;
+  switch(true){
+    case (height >= 300):
+      num = baseCirclesPerTitle * 2;
+      break;
+    default:
+      num = baseCirclesPerTitle;
+  }
+  return num;
+}
+
 function getCircleSize(){
-  return Math.floor((document.querySelector('.js-title').offsetHeight / circlesPerTitle) + 0);
+  return Math.floor(document.querySelector('.js-title').offsetHeight / getCirclesPerTitle());
 }
 
 function showFooter(){
   document.querySelector('footer').style.display = 'block';
 }
 
-function setAndShowTitle(circleSize){
-  let titleEl = document.querySelector('.js-title');
-  titleEl.style.marginTop = `${(circleSize * titleStart) - (circleMargin / 2)}px`;
-  titleEl.style.opacity = 1;
+function setAndShowTitle(){
+  anime({
+    targets: document.querySelector('.js-title'),
+    opacity: 1,
+    marginTop: `${(getCircleSize() * titleStart) - (circleMargin / 2)}px`,
+    loop: false,
+    duration: 500,
+    easing: 'easeInOutSine'
+  });
 }
 
-function handleResize(circleSize){
+function handleResize(){
   let pageWidth = window.outerWidth;
   let resize = debounce(function(){
     if(window.outerWidth !== pageWidth) {
@@ -98,7 +117,8 @@ function handleResize(circleSize){
       Array.prototype.forEach.call(document.querySelectorAll('.js-circle'), function(el){
         el.parentNode.removeChild(el);
       });
-      renderCircles(circleSize);
+      renderCircles(getCircleSize());
+      setAndShowTitle();
     }
   }, 400);
 
@@ -106,12 +126,11 @@ function handleResize(circleSize){
 }
 
 function init(){
-  var circleSize = getCircleSize();
-  renderCircles(circleSize);
-  setAndShowTitle(circleSize)
+  renderCircles();
+  setAndShowTitle()
   setupLinks();
   showFooter();
-  handleResize(circleSize);
+  handleResize();
 }
 
 export default init
